39ead01db957cca29c598fa8aea22ee7e8217aba
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-02-27 11:35:38 +0100

    Cleanup and simple usage of Razor Pages.

---
+++ week_01/learn_002_razor_application/Presentation.WebApp/Pages/Contact.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.WebApp.Pages;
+
+public class Contact : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+
+    public void OnPost()
+    {
+        
+    }
+
+    // public void OnPut()
+    // {
+    //     
+    // }
+    //
+    // public void OnDelete()
+    // {
+    //     
+    // }
+}
