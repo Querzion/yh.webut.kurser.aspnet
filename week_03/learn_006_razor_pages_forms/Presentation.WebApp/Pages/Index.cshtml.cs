f3c37bf9e08a0a1a072b3262a47a3c879fb6713e
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-10 14:32:59 +0100

    Why did you create stuff that you don't walk us through?

---
+++ week_03/learn_006_razor_pages_forms/Presentation.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Presentation.WebApp.Models;
+
+namespace Presentation.WebApp.Pages;
+
+public class IndexModel(ClientService clientService) : PageModel
+{
+    private readonly ClientService _clientService = clientService;
+    
+    [BindProperty] 
+    public RegisterUserModel FormData { get; set; } = new();
+    
+    public List<SelectListItem> ClientOptions { get; set; } = [];
+    
+    public async Task OnGet()
+    {
+        var clients = await _clientService.GetClientsAsync();
+        // Old way to write this
+        // ClientOptions = clients.Select(x => new SelectListItem
+        // {
+        //     Value = x.Id.ToString(),
+        //     Text = x.ClientName
+        // }).ToList();
+        
+        // New way to write this.
+        // ClientOptions = [.. clients.Select(x => new SelectListItem
+        // {
+        //    Value = x.Id.ToString(),
+        //    Text = x.ClientName
+        // })];
+    }
+    
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+            return Page();
+        
+        return RedirectToPage("/Index");
+    }
+}
