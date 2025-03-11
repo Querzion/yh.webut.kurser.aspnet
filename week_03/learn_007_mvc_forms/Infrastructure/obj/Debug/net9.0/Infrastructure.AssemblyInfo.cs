6fa39940f460fc13cb731ec0761acfd1edabf85a
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-11 11:38:12 +0100

    Regions for ChatGPT generated code. Everyday is a new way of thinking.

--- week_03/learn_007_mvc_forms/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ week_03/learn_007_mvc_forms/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0fb481d6d838ca74a69bfbc73c8e770cf0a9fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9b9613b5e9e9436901a1f6ea831f0b7f99d720")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
