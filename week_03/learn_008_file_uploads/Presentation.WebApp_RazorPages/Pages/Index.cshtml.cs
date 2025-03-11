d6bef8a26afb5b7e97f778c99bdafb4397a22f2d
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-11 13:29:51 +0100

    Projects are emptied and preset with cdn's instead of local files for bootstrap.

--- week_03/learn_008_file_uploads/Presentation.WebApp_RazorPages/Pages/Index.cshtml.cs
+++ week_03/learn_008_file_uploads/Presentation.WebApp_RazorPages/Pages/Index.cshtml.cs
@@ -5,12 +5,6 @@ namespace Presentation.WebApp_RazorPages.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
 
     public void OnGet()
     {
