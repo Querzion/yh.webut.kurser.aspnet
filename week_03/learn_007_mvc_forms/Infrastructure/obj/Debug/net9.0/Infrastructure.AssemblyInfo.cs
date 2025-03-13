2c4b30bd937e346dce6e38e45e4fd04068c9d224
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-13 19:00:55 +0100

    test

--- week_03/learn_007_mvc_forms/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ week_03/learn_007_mvc_forms/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a745e7d1dcba513256b144d327036bb2fb2bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970607386b6730e5fe6a6e15049d10b414aeaf71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
