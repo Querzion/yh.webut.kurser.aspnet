bcc4542d03504ea9ffcae1fb3d35709f35c91928
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-17 10:40:13 +0100

    identity scaffolding done in areas, the _layout file is not working correctly from the get go, neither does the app.db (had to delete it, create a new, and dotnet-ef update database, then it added the tables. The scaffolding on the other hand is for me a critical error in the template.

---
+++ week_04/learn_010_asp.net_identity/Presentation.WebApp_MVC_Individual_Authentication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Presentation.WebApp_MVC_Individual_Authentication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
