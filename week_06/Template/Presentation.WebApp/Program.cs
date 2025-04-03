f30cbec87d435c44bba7970c24fd51b342172d04
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-04-03 13:03:34 +0200

    Template build, normal registration.

---
+++ week_06/Template/Presentation.WebApp/Program.cs
@@ -0,0 +1,52 @@
+using Business.Services;
+using Data.Contexts;
+using Data.Entities;
+using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection")));
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/auth/signin";
+    options.AccessDeniedPath = "/auth/denied";
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+});
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+var app = builder.Build();
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("^$", "/admin/overview"));
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Overview}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
